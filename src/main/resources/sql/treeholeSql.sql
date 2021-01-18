d1c4e07bffa67c1904aa455834589000005f0c2f
git <2917021186@qq.com>
2021-01-18 10:41:09 +0800

    add:add test data
    添加了测试数据

--- src/main/resources/sql/treeholeSql.sql
+++ src/main/resources/sql/treeholeSql.sql
@@ -33,3 +33,18 @@ select * from `Message`;
 select * from `Message`;
 
 delete from `Message` where user_id=3;
+
+delete from `Message` where id < 15;
+
+delete from `User` where id < 15;
+
+insert into `User` values(0,'三分饱','12345678911','123456','https://wanfengcxz.cn/treehole/images/icons/avatar01.jpg');
+insert into `User` values(0,'狐狸先生和他的朋友','12345678922','123456','https://wanfengcxz.cn/treehole/images/icons/avatar02.jpg');
+insert into `User` values(0,'带带我鸭','12345678933','123456','https://wanfengcxz.cn/treehole/images/icons/avatar03.jpg');
+insert into `User` values(0,'鬼面人','12345678944','123456','https://wanfengcxz.cn/treehole/images/icons/avatar04.jpg');
+insert into `User` values(0,'一个人的旅行','12345678955','123456','https://wanfengcxz.cn/treehole/images/icons/avatar05.jpg');
+insert into `User` values(0,'柠檬汽水','12345678966','123456','https://wanfengcxz.cn/treehole/images/icons/avatar06.jpg');
+insert into `User` values(0,'带带大师兄','12345678977','123456','https://wanfengcxz.cn/treehole/images/icons/sxc.png');
+
+insert into `Message` values(0,15,'Amazing','aaa.jpg','今天天气真不错',0,1234567891);
+
